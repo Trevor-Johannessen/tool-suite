b7af3f59fd12a02d7b2947e38b12c6a82476961f
Trevor Johannessen <trevor.johannessen@gmail.com>
2025-03-18 19:02:07 -0400

    Added template for commands

--- mcsm.sh
+++ mcsm.sh
@@ -45,6 +45,16 @@ COMMANDS:
 	create {server_name} - Creates a new server.
 	delete {server_name} - Deletes a server and its files.
 	list - Lists all registered servers.
+	command {server_name} {command} - Runs a command on a given server
+	kick {server_name} {player} - Kicks a player from a running server
+	ban {server_name} {player} - Bans a player from a running server
+	pardon {server_name} {player} - Pardons a player from a running server
+	mute {server_name} {player} - Mutes a player on a given server
+	unmute {server_name} {player} - Unmutes a player on a given server
+	whitelist {server_name} {player} - Adds a player to the server's whitelist.
+	blacklist {server_name} {player} - Removed a player from a server's whitelist.
+	render {server_name} {world} {radius} {x} {z} - Renders a circle around the given coordinates for the given world.
+	
 	help - Prints the help menu.
 	ping - pong!
 	"
@@ -52,6 +62,22 @@ COMMANDS:
 
 print_headers(){
 	echo "Name|Filepath|Last Start|Total Starts|Exclusions"
+}
+
+server_run_command(){
+	# Verify the server exists
+	server_verify $1
+        if [ $? -ne 2 ]; then
+        	return 1
+        fi
+	
+	# Find the server and print it's attributes
+	server=$(sq	lite3 "$LOCAL_FILES/servers.db" "SELECT * FROM servers WHERE name = '$1';")
+	
+	# Get screen the given server is running on
+
+	# Send command to found screen
+
 }
 
 server_running(){
@@ -256,6 +282,118 @@ server_delete(){ # (name)
 	mcsm_print $verbose "$1 deleted."
 }
 
+command_kick(){
+	# Get server file location
+	server_location=$(sqlite3 "$LOCAL_FILES/servers.db" "SELECT filepath FROM servers WHERE name = '$1';")	
+	if [ "$server_location" = "" ]; then
+		mcsm_print $error "Could not find server $1."
+		exit 1
+	fi
+
+
+
+}
+
+command_ban(){
+	# Get server file location
+	server_location=$(sqlite3 "$LOCAL_FILES/servers.db" "SELECT filepath FROM servers WHERE name = '$1';")	
+	if [ "$server_location" = "" ]; then
+		mcsm_print $error "Could not find server $1."
+		exit 1
+	fi
+
+
+
+}
+
+
+command_pardon(){
+	# Get server file location
+	server_location=$(sqlite3 "$LOCAL_FILES/servers.db" "SELECT filepath FROM servers WHERE name = '$1';")	
+	if [ "$server_location" = "" ]; then
+		mcsm_print $error "Could not find server $1."
+		exit 1
+	fi
+
+
+
+}
+
+command_mute(){
+	# Get server file location
+	server_location=$(sqlite3 "$LOCAL_FILES/servers.db" "SELECT filepath FROM servers WHERE name = '$1';")	
+	if [ "$server_location" = "" ]; then
+		mcsm_print $error "Could not find server $1."
+		exit 1
+	fi
+
+
+
+}
+
+
+command_unmute(){
+	# Get server file location
+	server_location=$(sqlite3 "$LOCAL_FILES/servers.db" "SELECT filepath FROM servers WHERE name = '$1';")	
+	if [ "$server_location" = "" ]; then
+		mcsm_print $error "Could not find server $1."
+		exit 1
+	fi
+
+
+
+}
+
+command_render(){
+	# Get server file location
+	server_location=$(sqlite3 "$LOCAL_FILES/servers.db" "SELECT filepath FROM servers WHERE name = '$1';")	
+	if [ "$server_location" = "" ]; then
+		mcsm_print $error "Could not find server $1."
+		exit 1
+	fi
+
+
+
+}
+
+command_say(){
+	# Get server file location
+	server_location=$(sqlite3 "$LOCAL_FILES/servers.db" "SELECT filepath FROM servers WHERE name = '$1';")	
+	if [ "$server_location" = "" ]; then
+		mcsm_print $error "Could not find server $1."
+		exit 1
+	fi
+
+
+
+}
+
+command_whitelist(){
+	# Get server file location
+	server_location=$(sqlite3 "$LOCAL_FILES/servers.db" "SELECT filepath FROM servers WHERE name = '$1';")	
+	if [ "$server_location" = "" ]; then
+		mcsm_print $error "Could not find server $1."
+		exit 1
+	fi
+
+
+
+}
+
+command_blacklist(){
+	# Get server file location
+	server_location=$(sqlite3 "$LOCAL_FILES/servers.db" "SELECT filepath FROM servers WHERE name = '$1';")	
+	if [ "$server_location" = "" ]; then
+		mcsm_print $error "Could not find server $1."
+		exit 1
+	fi
+
+
+
+}
+
+
+
 # Check if sqlite3 exists and has the correct database setup
 if [ ! -d "$LOCAL_FILES" ]; then
 	mcsm_print $error "The minecraft server manager directory has not been created. Try running mcsm install."
@@ -346,6 +484,30 @@ case $command in
 		;;
 	"backup")
 		server_backup $1 $2
+		;;
+	"mute")
+		command_mute $1 $2
+		;;
+	"unmute")
+		command_unmute $1 $2
+		;;
+	"ban")
+		command_ban $1 $2
+		;;
+	"pardon")
+		command_pardon $1 $2
+		;;
+	"whitelist")
+		command_whitelist $1 $2
+		;;
+	"blacklist")
+		command_blacklist $1 $2
+		;;
+	"say")
+		command_say $1 $2
+		;;
+	"render")
+		command_render $1 $2
 		;;
 	# Testing command
 	"ping")
